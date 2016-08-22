01842aef4af57ae90485e4007625a6a85ad4dc9c
johnny01.yang <johnny01.yang@vipshop.com>
2016-08-22 11:58:23 +0800

    1.HttpUtils 添加文件上传功能

---
+++ src/main/java/com/johnny/common/constant/CommonConstants.java
@@ -0,0 +1,28 @@
+package com.johnny.common.constant;
+
+/**
+ * Description: This CommonConstants is used to define common constants unrelated with Business Logic
+ * <p>
+ * Author: johnny01.yang
+ * Date  : 2016-08-01 11:47
+ */
+public final class CommonConstants {
+
+    private CommonConstants() {
+    }
+
+    public static final String MINUS = "-";
+    public static final String COLON = ":";
+    public static final String BLANK = "";
+    public static final String EMPTY = "";
+    public static final String QUESTION_MARK = "?";
+    public static final String AND = "&";
+    public static final String EQUALS = "=";
+    public static final String SQUARE_BRACKETS_LEFT = "[";
+    public static final String SQUARE_BRACKETS_RIGHT = "]";
+    public static final String COMMA = ",";
+
+    public static final String SUCCESS_CODE = "200";
+    public static final String CLIENT_ERROR_CODE = "400";
+    public static final String SERVER_ERROR_CODE = "500";
+}
--- src/main/java/com/johnny/common/utils/http/HttpUtils.java
+++ src/main/java/com/johnny/common/utils/http/HttpUtils.java
@@ -1,96 +1,84 @@
 package com.johnny.common.utils.http;
 
-import com.google.common.collect.Lists;
+import com.johnny.common.constant.CommonConstants;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.impl.WeakHashtable;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * Created by johnny01.yang on 2016/6/30.
+ * Description:  * This HttpUtils delegate to Apache HttpComponents to execute GET|POST request
+ * and HttpMime to process multipart upload request
+ * see <a href="https://hc.apache.org/">https://hc.apache.org/</a>
+ * Author: johnny01.yang
+ * Date  : 2016-08-22 11:47
  */
 public class HttpUtils {
 
     private HttpUtils() {
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(HttpUtils.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpUtils.class);
 
-    public static final int DEFAULT_TIMEOUT = 50000;
-
-    public static final String DEFAULT_CHARSET = "UTF-8";
-
-    public static final String DEFAULT_ACCEPT = "application/json";
-
-    public static final int WRONG_CODE = 400;
-
-    public static final String QUESTION_MARK = "?";
-
-    public static final String AND = "&";
-
-    public static final String EQUALS = "=";
-
-    private static final String BLANK = "";
+    private static final int DEFAULT_TIMEOUT = 50000;
+    private static final String DEFAULT_CHARSET = "UTF-8";
+    private static final String DEFAULT_ACCEPT = "application/json";
+    private static final int WRONG_CODE = 400;
 
     /**
      * Using Get method to execute a request
      *
-     * @param path eg:http://www.baidu.com?a=1&b=2
+     * @param path eg:http://www.abc.com?a=1&b=2
      * @return json result
      */
     public static String executeGetMethodRequest(String path) {
         String result = null;
-        HttpClient httpClient;
-        HttpGet httpGet;
+        HttpGet httpGet = null;
         try {
             URL url = new URL(path);
-            URI uri = new URI(url.getProtocol(), null, url.getHost(), url.getPort(), url.getPath(), url.getQuery(), null);
+            URI uri = new URI(url.getProtocol(), null, url.getHost(), url.getPort(), url.getPath(),
+                    url.getQuery(), null);
             httpGet = new HttpGet(uri);
             httpGet.addHeader("accept", DEFAULT_ACCEPT);
 
-            httpClient = getPooledHttpClient(DEFAULT_TIMEOUT);
-
+            HttpClient httpClient = getPooledHttpClient(DEFAULT_TIMEOUT);
             HttpResponse response = httpClient.execute(httpGet);
             if (response.getStatusLine().getStatusCode() >= WRONG_CODE) {
-                logger.warn("Http Request Failed: Error Code" + response.getStatusLine().getStatusCode());
+                LOGGER.warn("Http request failed-- URL:".concat(path).concat(", ErrorCode:").concat(
+                        String.valueOf(response.getStatusLine().getStatusCode())));
+            } else {
+                result = fetchContent(response.getEntity().getContent());
             }
-            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), DEFAULT_CHARSET));
-            StringBuilder s = new StringBuilder();
-            while ((result = br.readLine()) != null) {
-                s = s.append(result);
-            }
-            result = s.toString();
-            br.close();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            LOGGER.error(e.getMessage(), e);
         } catch (URISyntaxException e) {
-            e.printStackTrace();
+            LOGGER.error(e.getMessage(), e);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            if (null != httpGet) {
+                httpGet.abort();
+            }
         }
         return result;
     }
@@ -98,35 +86,29 @@ public class HttpUtils {
     /**
      * Using POST method to execute a request
      *
-     * @param url      eg:http://www.baidu.com
+     * @param path     eg:http://www.aaa.com
      * @param paramMap paramName: paramValue
      * @return json result
      */
-    public static String executePostMethodRequest(String url, Map<String, String> paramMap) {
+    public static String executePostMethodRequest(String path, Map<String, String> paramMap) {
         String result = null;
         HttpPost httpPost = null;
         try {
-            httpPost = new HttpPost(url);
-            httpPost.addHeader("accept", DEFAULT_ACCEPT);
+            httpPost = new HttpPost(path);
             List<NameValuePair> paramList = constructNameValuePair(paramMap);
             httpPost.setEntity(new UrlEncodedFormEntity(paramList, DEFAULT_CHARSET));
             HttpClient httpClient = getPooledHttpClient(DEFAULT_TIMEOUT);
             HttpResponse response = httpClient.execute(httpPost);
-            if (response.getStatusLine().getStatusCode() >= 400) {
-                logger.error("Http Request Failed: Error Code" + response.getStatusLine().getStatusCode());
+            if (response.getStatusLine().getStatusCode() >= WRONG_CODE) {
+                LOGGER.error("Http Request Failed: URL".concat(path).concat(",Params:").concat(paramMap.toString())
+                        .concat(",Error Code").concat(String.valueOf(response.getStatusLine().getStatusCode())));
+            } else {
+                result = fetchContent(response.getEntity().getContent());
             }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), DEFAULT_CHARSET));
-            StringBuilder s = new StringBuilder();
-            while ((result = br.readLine()) != null) {
-                s = s.append(result);
-            }
-            result = s.toString();
-            br.close();
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            LOGGER.error(e.getMessage(), e);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error(e.getMessage(), e);
         } finally {
             if (null != httpPost) {
                 httpPost.abort();
@@ -135,8 +117,32 @@ public class HttpUtils {
         return result;
     }
 
+    /**
+     * This method is equals to {@link org.apache.http.util.EntityUtils#toString(HttpEntity, Charset)}
+     *
+     * @param content HttpEntity.getContent()
+     * @return content
+     */
+    private static String fetchContent(InputStream content) {
+        String result = null;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(content, DEFAULT_CHARSET))) {
+            StringBuilder subLine = new StringBuilder("");
+            String line = br.readLine();
+            while (null != line) {
+                subLine = subLine.append(line);
+                line = br.readLine();
+            }
+            result = subLine.toString();
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return result;
+    }
+
     private static List<NameValuePair> constructNameValuePair(Map<String, String> paramMap) {
-        List<NameValuePair> pairs = new ArrayList<>();
+        List<NameValuePair> pairs = new ArrayList<>(paramMap.size());
         for (Map.Entry<String, String> entry : paramMap.entrySet()) {
             pairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
         }
@@ -144,48 +150,114 @@ public class HttpUtils {
     }
 
     /**
-     * @param timeout : connectionTimeout | sotimeout
+     * @param timeout : connectionTimeout | timeout
      */
-    public static HttpClient getPooledHttpClient(int timeout) {
-        RequestConfig rconf = RequestConfig.custom().setConnectTimeout(timeout).setSocketTimeout(timeout).build();
-        return HttpClientBuilder.create().setDefaultRequestConfig(rconf).build();
+    private static HttpClient getPooledHttpClient(int timeout) {
+        RequestConfig resConf = RequestConfig.custom().setConnectTimeout(timeout).setSocketTimeout(timeout).build();
+        return HttpClientBuilder.create().setDefaultRequestConfig(resConf).build();
     }
 
     /**
      * 拼接URL和请求参数
      *
-     * @param baseUrl  eg:http://www.baidu.com?
-     * @param paramMap {a-> null, b-> 3}
-     * @return http://www.baidu.com?a=&b=3
+     * @param baseUrl  eg:http://www.aaa.com?
+     * @param paramMap {a-> 1, b-> 3}
+     * @return http://www.aaa.com?a=1&b=3
+     * @deprecated Use {@link HttpUtils#appendFmtRequestParam(String, Map<String, Object>)} instead. This
+     * method is scheduled for removal which ignore params(key and value) with Chinese or paramValue is null
      */
-    public static String appendRequestParam(String baseUrl, Map<String, Object> paramMap) throws UnsupportedEncodingException {
+    @Deprecated
+    public static String appendRequestParam(String baseUrl, Map<String, Object> paramMap) {
+        StringBuilder fmtUrl = new StringBuilder(baseUrl);
+        if (StringUtils.isNotBlank(baseUrl)) {
+            if (!baseUrl.endsWith(CommonConstants.QUESTION_MARK)) {
+                fmtUrl = fmtUrl.append(CommonConstants.QUESTION_MARK);
+            }
+            for (Map.Entry<String, Object> entry : paramMap.entrySet()) {
+                fmtUrl = fmtUrl.append(entry.getKey()).append(CommonConstants.EQUALS)
+                        .append(entry.getValue()).append(CommonConstants.AND);
+            }
+        }
+        return fmtUrl.substring(0, fmtUrl.length() - 1);
+    }
+
+    /**
+     * 拼接URL和请求参数. This method is a replace for {@link HttpUtils#appendRequestParam(String, Map<String, Object>)}
+     *
+     * @param baseUrl  eg:http://www.aaa.com?
+     * @param paramMap key and value map
+     * @return responseStr
+     */
+    public static String appendFmtRequestParam(String baseUrl, Map<String, Object> paramMap) throws UnsupportedEncodingException {
         String fmtUrl = baseUrl;
         if (StringUtils.isBlank(baseUrl)) {
             return null;
         }
-        if (!baseUrl.endsWith(QUESTION_MARK)) {
-            fmtUrl = fmtUrl.concat(QUESTION_MARK);
+        if (!baseUrl.endsWith(CommonConstants.QUESTION_MARK)) {
+            fmtUrl = fmtUrl.concat(CommonConstants.QUESTION_MARK);
         }
         for (Map.Entry<String, Object> entry : paramMap.entrySet()) {
             if (StringUtils.isBlank(entry.getKey())) {
                 continue;
             }
             String fmtKey = URLEncoder.encode(entry.getKey(), DEFAULT_CHARSET);
-            String fmtValue = BLANK;
+            String fmtValue = CommonConstants.BLANK;
             if (null != entry.getValue()) {
                 fmtValue = URLEncoder.encode(entry.toString(), DEFAULT_CHARSET);
             }
-            fmtUrl = fmtUrl.concat(fmtKey).concat(EQUALS).concat(fmtValue).concat(AND);
+            fmtUrl = fmtUrl.concat(fmtKey).concat(CommonConstants.EQUALS).concat(fmtValue).concat(CommonConstants.AND);
         }
         return fmtUrl.substring(0, fmtUrl.length() - 1);
     }
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String baseUrl = "http://www.baidu.com";
-        Map<String, Object> map = new HashMap<>();
-        map.put("1", null);
-        map.put("2", "aa");
-        System.out.println(HttpUtils.appendRequestParam(baseUrl, map));
-    }
+    /**
+     * execute multipart upload
+     * <p>
+     * <pre>{@code
+     * String path = "http://xg-file.api.vip.com/statics/file/upload";
+     * Map<String, File> fileMap = new HashMap<>();
+     * fileMap.put("file", new File("D:\\build.gradle"));
+     * Map<String, String> paramMap = new HashMap<>();
+     * paramMap.put("domain", "mxfd.vipstatic.com");
+     * String responseStr = HttpUtils.executePostPartRequest(path, fileMap, paramMap);
+     * }</pre>
+     *
+     * @param path     eg:http://www.aaa.com?
+     * @param fileMap  files to upload
+     * @param paramMap extra param
+     * @return responseStr
+     */
+    public static String executePostPartRequest(String path, Map<String, File> fileMap, Map<String, String> paramMap) {
+        String result = null;
+        HttpClient client;
+        HttpPost httpPost;
+        try {
+            httpPost = new HttpPost(path);
+            client = getPooledHttpClient(DEFAULT_TIMEOUT);
 
+            MultipartEntityBuilder mBuilder = MultipartEntityBuilder.create();
+            for (Map.Entry<String, File> entry : fileMap.entrySet()) {
+                mBuilder.addPart(entry.getKey(), new FileBody(entry.getValue()));
+            }
+            for (Map.Entry<String, String> entry : paramMap.entrySet()) {
+                mBuilder.addPart(entry.getKey(), new StringBody(entry.getValue(), ContentType.APPLICATION_FORM_URLENCODED));
+            }
+            HttpEntity httpEntity = mBuilder.build();
+            httpPost.setEntity(httpEntity);
+            HttpResponse response = client.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() >= WRONG_CODE) {
+                LOGGER.error("Http Request Failed: URL".concat(path).concat(",Params:").concat(paramMap.toString())
+                        .concat(",Error Code").concat(String.valueOf(response.getStatusLine().getStatusCode())));
+            } else {
+                result = fetchContent(response.getEntity().getContent());
+                //equals to following code snippet
+                //result = EntityUtils.toString(response.getEntity(), DEFAULT_CHARSET);
+            }
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return result;
+    }
 }
